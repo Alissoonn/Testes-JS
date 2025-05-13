252834dba1a802dd234a9a16793069eb01bfc443
Alisson Dias <alissondias1523@gmail.com>
2025-05-13 14:30:22 -0300

    finish

---
+++ desafio1.js
@@ -0,0 +1,11 @@
+function exibir1(){
+    window.alert("Voce clicou no botão 1")
+}
+
+function exibir2(){
+    window.alert("Voce clicou no botão 2")
+}
+
+function exibir3(){
+    window.alert("Voce clicou no botão 3")
+}
