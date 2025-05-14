f752465297b55331fbb886535940668942492459
Alisson Dias <alissondias1523@gmail.com>
2025-05-14 20:33:17 -0300

    att

---
+++ DESAFIO BOAS VINDAS/desafio.js
@@ -0,0 +1,5 @@
+function exibir(){
+   let nome =  window.prompt("Qual o seu nome?")
+   let idade =  window.prompt("Quantos anos você tem?")
+   window.alert(`Acabei de conheçer o ${nome} que tem ${idade} anos`)
+}
