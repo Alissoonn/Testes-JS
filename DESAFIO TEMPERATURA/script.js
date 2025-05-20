c8d92c5b9fa2ae0c0fc650d6db8d4924bf7c5cf7
Alisson Dias <alissondias1523@gmail.com>
2025-05-20 14:23:52 -0300

    att

--- DESAFIO TEMPERATURA/script.js
+++ DESAFIO TEMPERATURA/script.js
@@ -1,3 +1,16 @@
 function temp(){
-    /*Falta fazer somente a função para converter a temperatura */
+  let cel = window.Number(parseFloat(prompt("Digite a temperatura em °C (Celsius)")))
+  let res = document.getElementById('txt')
+
+  if(isNaN(cel)){//SE cel NÂO FOR UM NUMERO,executa o Inner abaixo
+    res.innerHTML = "Por favor digite um número valido"
+    return;
+  }
+
+let kel = cel + 273.15;/*Formula para graus Kelvin*/
+let far = (cel * 9/5) + 32;/*Formula para graus fahren*/
+  
+res.innerHTML = (`A temperatura de ${cel} corresponde a...<br> ${kel.toFixed(2)}°K (Kelvin) e ${far.toFixed(2)}°F (Fahrenheit)`)
 }
+
+
