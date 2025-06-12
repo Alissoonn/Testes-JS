525a7ae893862a5f5ca2734cd3f6dea4fb39cdbd
Alisson Dias <alissondias1523@gmail.com>
2025-06-12 10:32:31 -0300

    att

--- DESAFIO REAJUSTE/script.js
+++ DESAFIO REAJUSTE/script.js
@@ -20,7 +20,24 @@ function re(){
     //Pega o salario atual e multiplica pelo aumento, depois divide em 100%
     let reajust = (sal*aumento) / 100;
     let novSal = sal+reajust;
+    
+    //Alterando o H2 
+    document.getElementById("h2").innerHTML = `${nome} recebeu um aumento de salário`
 
-    //Alterando o paragrafo 
-    document.getElementById("txt").innerHTML = `O salário atual era de R$${sal.toFixed(2)}`
+    //Cria um paragrafo dentro do HTML
+    let p1 = document.createElement("p");
+    p1.id="par1"
+    p1.textContent = `O salario anterior era de R$${sal.toFixed(2)}`
+    document.body.appendChild(p1)//Insere o paragrafo dentro de algum conteudo no HTML (dentro do body é o mais comum)
+
+    let p2 = document.createElement("p");
+    p2.id="par2"
+    p2.textContent = `Com o aumento de ${aumento}%, seu salario vai aumentar R$${reajust.toFixed(2)} no proximo mês`
+    document.body.appendChild(p2)//Insere o paragrafo dentro de algum conteudo no HTML (dentro do body é o mais comum)
+
+
+    let p3 = document.createElement("p");
+    p3.id="par3";
+    p3.textContent = `A partir do mês seguinte, ${nome} passa a receber R$${novSal.toFixed(2)}`
+    document.body.appendChild(p3)//Insere o paragrafo dentro de algum conteudo no HTML (dentro do body é o mais comum)
 }
