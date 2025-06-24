42cda26443f745b7120b6521f6a6cff096218918
Alisson Dias <alissondias1523@gmail.com>
2025-06-24 13:48:43 -0300

    att

---
+++ DESAFIO PRECO MUDOU/script.js
@@ -0,0 +1,4 @@
+function veri(){
+    let PreAnt = window.parseFloat(prompt("Qual era o preço anterior do produto?"))
+    let PreAtu = window.parseFloat(prompt("Qual o preço atual do produto? "))
+}
