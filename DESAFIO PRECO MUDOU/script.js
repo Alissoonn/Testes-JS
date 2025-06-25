b7faa32435fca4fbe5f94e2e3b95bbfb966398f4
Alisson Dias <alissondias1523@gmail.com>
2025-06-25 13:12:56 -0300

    update

--- DESAFIO PRECO MUDOU/script.js
+++ DESAFIO PRECO MUDOU/script.js
@@ -1,4 +1,16 @@
 function veri(){
     let PreAnt = window.parseFloat(prompt("Qual era o preço anterior do produto?"))
+    //Valida se ambos os prompts receberam o valor corretos
+    if(isNaN(PreAnt)){
+        alert("Digite um valor válido");
+        return;
+    }
+
     let PreAtu = window.parseFloat(prompt("Qual o preço atual do produto? "))
+    //Valida se ambos os prompts receberam o valor corretos
+    if(isNaN(PreAtu)){
+        alert("Digite um valor válido");
+        return;
+    }
+
 }
